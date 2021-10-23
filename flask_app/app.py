603890680d4fb315d3d0fb86638cadb31f11220d
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2021-10-23 18:52:15 -0400

    added the model

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,42 +1,43 @@
-from flask import Flask, render_template, request, url_for , jsonify
-from model.recomendation import recommend
+from flask import Flask, render_template, request, url_for , jsonify, redirect
+import pandas as pd
+from model.recomender import recomend
+
 
 app = Flask(__name__)
 
 model_input = ""
+TFIDF_ENCODING_PATH = './data/models/tfidf_encodings.pkl'
+TFIDF_MODEL_PATH = './data/models/tfidf.pkl'
+RECIPES_PATH = './data/kaggle/RAW_recipes.csv'
+PARSED_PATH = './data/kaggle/df_parsed.csv'
+N = 100
+
+@app.before_request
+def before_request_func():
+    print("before_request is running!")
 
 # home: show all the interface we have, can take in input
 @app.route("/")
 def home():
     return render_template('homepage.html')
 
-@app.route('/submit', methods=['POST' , 'GET'])
+@app.route('/submit', methods=['POST'])
 def submit():
-    if request.method == "POST":
-        grocery_list = request.form.get('groceryList')
-        # clean up grocery list in to an array string for our model 
-        grocery_arr = grocery_list.split(',')
-        model_input = "["
-        for i in range(len(grocery_arr)):
-            if(i == len(grocery_arr) -1):
-                model_input = model_input + grocery_arr[i]
-            else: 
-                model_input = model_input + grocery_arr[i] + ","
-        model_input = model_input + "]"
-        return "Your List:" + model_input
-    else: 
-        recipe = recommend(model_input)
-        
-        # We need to turn output into JSON. 
-        response = {}
-        count = 0    
-        for index, row in recipe.iterrows():
-            response[count] = {
-                                'recipe': str(row['recipe']),
-                                'score': str(row['score']),
-                                'ingredients': str(row['ingredients']),
-                                'url': str(row['url'])
-                            }
-            count += 1
-        return jsonify(response)
+    grocery_list = request.form.get('groceryList')
+    # clean up grocery list in to an array string for our model 
+    grocery_arr = grocery_list.split(',')
+    model_input = "["
+    for i in range(len(grocery_arr)):
+        if(i == len(grocery_arr) -1):
+            model_input = model_input + grocery_arr[i]
+        else: 
+            model_input = model_input + grocery_arr[i] + ","
+    model_input = model_input + "]"
+    return redirect((url_for('result')))
+
+@app.route('/result', methods=['GET'])
+def result():
+    recipe = recomend(RECIPES_PATH, TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH, model_input , N)
+    # write the recipe in a file so that we dont need to load it again in the filtering
+    return (recipe.to_json)
 
--- flask_app/model/recomender.py
+++ flask_app/model/recomender.py
@@ -1,29 +1,36 @@
+from flask import jsonify
 from numpy import DataSource
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
-# from flask_app.model.helpers import ogdataset
-# from flask_app.model.helpers import compare_input, extract_feature, get_recommendations
+from model.helpers import ogdataset
+from model.helpers import compare_input, extract_feature, get_recommendations
 
-# from flask_app.model.helpers import dataset
-from helpers import ogdataset, dataset, compare_input, extract_feature, get_recommendations
+from model.helpers import dataset
+# from helpers import ogdataset, dataset, compare_input, extract_feature, get_recommendations
 
 
 def recomend(RECIPES_PATH, TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH, ingredients, N):
     og_df = ogdataset(RECIPES_PATH)
+    print("Getting Dataset...")
     new_df = dataset(og_df)
+    print("Extracting Feature...")
     extract_feature(new_df,TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH )
+    print("Counting Score...")
     scores = compare_input(ingredients,TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH )
+    print("Getting your recommendation...")
     recomendations = get_recommendations(og_df, N, scores)
-    print(recomendations.head())
     return recomendations
 
-if __name__ == "__main__":
-    TFIDF_ENCODING_PATH = '../../data/models/tfidf_encodings.pkl'
-    TFIDF_MODEL_PATH = '../../data/models/tfidf.pkl'
-    RECIPES_PATH = '../../data/kaggle/RAW_recipes.csv'
-    PARSED_PATH = '../../data/kaggle/df_parsed.csv'
-    ingredients = '["bacon", "eggs", "bread"]'
-    N = 100
-    recomend(RECIPES_PATH, TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH, ingredients , N)
+# if __name__ == "__main__":
+#     TFIDF_ENCODING_PATH = '../data/models/tfidf_encodings.pkl'
+#     TFIDF_MODEL_PATH = '../data/models/tfidf.pkl'
+#     RECIPES_PATH = '../data/kaggle/RAW_recipes.csv'
+#     PARSED_PATH = '../data/kaggle/df_parsed.csv'
+#     ingredients = '["bacon", "eggs", "bread"]'
+#     N = 100
+#     result = recomend(RECIPES_PATH, TFIDF_MODEL_PATH, TFIDF_ENCODING_PATH, ingredients , N)
+#     print("Done\n")
+#     print(result)
+
