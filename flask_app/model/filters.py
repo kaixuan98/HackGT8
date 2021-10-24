1c015fafbfe36bdd9977e16d03a717b7f154e26f
Sherelene De Belen <sherelenedebelen@gmail.com>
2021-10-23 17:28:58 -0700

    first version of filters

---
+++ flask_app/model/filters.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+def transformed_nutrition(data_frame):
+    new_df = data_frame.copy()
+
+    # make a new data frame with the nutrition values split up
+    new_df['calories'], new_df['total fat (PDV)'], new_df['sugar (PDV)'], new_df['sodium (PDV)'], new_df[
+        'protein (PDV)'], new_df['saturated fat (PDV)'], new_df['carbohydrates (PDV)'] = new_df['nutrition'].str.split(
+        ',', 6).str
+
+    del new_df['nutrition']  # delete old nutrition column since we split values into their own column
+    del new_df['id']  # delete id column since we dont need them
+
+    # clean up calories and carbs so it doesnt have the "[" in the rows
+    # plus convert to float
+    new_df['calories'] = new_df['calories'].str.replace(r'\D', '').astype(float)
+    new_df['carbohydrates (PDV)'] = new_df['carbohydrates (PDV)'].str.replace(r'\D', '').astype(float)
+
+    # change all the nutrition columns into floats instead of string
+    # calories and carbs were converted earlier so thats why its not included now
+
+    pd.to_numeric(new_df['total fat (PDV)'])
+    pd.to_numeric(new_df['sugar (PDV)'])
+    pd.to_numeric(new_df['sodium (PDV)'])
+    pd.to_numeric(new_df['protein (PDV)'])
+    pd.to_numeric(new_df['saturated fat (PDV)'])
+
+    return new_df
+
+
+# filters out from list if it contains allergen ingredient
+def allergy(allergen, data_frame):
+    no_allergen_df = data_frame.copy()
+
+    df_copy = no_allergen_df[~no_allergen_df.ingredients.str.contains(allergen)]
+
+    return df_copy
+
+
+def nutrition_range(inequality, range_num, nutrition_name, data_frame):
+    new_df = data_frame.copy()
+    if inequality == "less than or equal to":
+        index_names = new_df[new_df[nutrition_name] > range_num].index
+        new_df.drop(index_names, inplace=True)
+    elif inequality == "more than or equal to":
+        index_names = new_df[new_df[nutrition_name] < range_num].index
+        new_df.drop(index_names, inplace=True)
+    else:
+        return new_df
